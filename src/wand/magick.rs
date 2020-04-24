8c8e0ce4edb37736ff7912de4b50756b957aa89c
Daniel Rönnkvist <daniel.ronnkvist@storykit.io>
2020-04-24 13:41:14 +0200

    feat: MagickSetImageAlpha

--- src/wand/magick.rs
+++ src/wand/magick.rs
@@ -765,6 +765,9 @@ impl MagickWand {
         MagickTransformImageColorspace => transform_image_colorspace(
             colorspace: bindings::ColorspaceType)
 
+        // Sets the image to the specified alpha level.
+        MagickSetImageAlpha => set_image_alpha(alpha: f64)
+
         /// Set the image alpha channel mode.
         MagickSetImageAlphaChannel => set_image_alpha_channel(
             alpha_channel: bindings::AlphaChannelOption)
