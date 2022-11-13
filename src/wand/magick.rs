d123b95b9e135c2f0dbaff9c99986673ede90da6
Maks Rawski <maksymilian.rawski@tutanota.com>
2022-11-13 18:41:07 +0100

    change 2 normal comments in magick.rs into docstrings

--- src/wand/magick.rs
+++ src/wand/magick.rs
@@ -986,10 +986,10 @@ impl MagickWand {
         MagickTransformImageColorspace => transform_image_colorspace(
             colorspace: bindings::ColorspaceType)
 
-        // Sets the image to the specified alpha level.
+        /// Sets the image to the specified alpha level.
         MagickSetImageAlpha => set_image_alpha(alpha: f64)
 
-        // Control the brightness, saturation, and hue of an image
+        /// Control the brightness, saturation, and hue of an image
         MagickModulateImage => modulate_image(brightness: f64, saturation: f64, hue: f64)
 
         /// Control the brightness and contrast
